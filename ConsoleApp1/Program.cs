19e46db97c1b9248fc8cbfbf1f3bcb71b11863ae
Finlay Collins <17ColliF@fulford.york.sch.uk>
2022-09-09 14:58:09 +0100

    wingus stingus

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+            Console.WriteLine("Number plz and I will repeat");
+            int reply = (int.Parse(Console.ReadLine()));
+            
+            for(int i = 1; i < reply + 1; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
         }
     }
 }
